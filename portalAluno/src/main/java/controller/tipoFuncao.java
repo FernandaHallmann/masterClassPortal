6479625c70f8527721d151641377eb0b7784a621
João Vitor Vieira <84626557+joaovflorisvaldo@users.noreply.github.com>
2023-03-22 20:10:42 -0300

    criando tipo funcao (#2)

---
+++ portalAluno/src/main/java/controller/tipoFuncao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author aluno
+ */
+public class tipoFuncao {
+    public String nomefuncao;
+    public int codigofuncao;
+    public String setorfuncao;
+    
+    public void cadastrarFuncao() {
+
+    }
+    public void cadastrarSetor() {
+
+    } 
+}
