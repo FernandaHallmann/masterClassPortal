b65fe7fe93005052aad80a3b6c8e7ccbe12a42a4
Eduardo Rodrigues <126730694+Eduardo-Rodigues@users.noreply.github.com>
2023-03-22 20:24:07 -0300

    Criar Classe Curso
    Criar classe curso. Adicionar atributos e métodos.

---
+++ portalAluno/src/main/java/controller/Curso.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curso;
+
+/**
+ *
+ * @author eduro
+ */
+public class Curso {
+    private int cargaHoraria;
+    private String disciplina;
+    private String turnoCurso;
+    private String campusAcademico;
+    
+    public void cadastrarCurso(){
+    
+    }
+    public void removerCurso(){
+    
+    }
+    public void cadastrarCargaHoraria(){
+    
+    }
+    public void cadastrarTurno(){
+    
+    }
+    public void cadastrarTurma(){
+    
+    }
+    public void editarTurno(){
+    
+    }
+    public void editarTurma(){
+    
+    }
+    public void editarCargaHoraria(){
+    
+    }
+}
