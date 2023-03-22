b76e905aaa294e6011a12ea5f91e4119a60ee219
BrunoStefanello <126917468+BrunoStefanello@users.noreply.github.com>
2023-03-22 20:39:22 -0300

    criando classe Instituição

---
+++ portalAluno/src/main/java/controller/Instituicao.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.instituicao;
+
+/**
+ *
+ * @author Bruno Stefanello
+ */
+public class Instituicao {
+
+    public int keyAluno;
+    public boolean aluno;
+    public boolean financeiro;
+    public boolean curso;
+    public boolean lagradouro;
+    public boolean biblioteca;
+    public boolean juridico;
+    public String emailInstituicao;
+
+    public void cadastrarCurso() {
+    }
+
+    public void cadastrarAluno() {
+    }
+
+    public void cadastrarFuncionario() {
+    }
+
+    public void cadastrarFinanceiro() {
+    }
+
+    public void cadastrarLivro() {
+    }
+
+    public void cadastrarEmail() {
+    }
+}
