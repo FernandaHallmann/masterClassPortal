0bc9a4b5e7c6f2d6cfcffc0d1e27da447b17f927
João Contini <105074710+JoaoMContini@users.noreply.github.com>
2023-03-22 20:05:27 -0300

    Criação das classes Aluno e Forma de Pagamento (#1)
    Criação das classes Aluno e FormaPagamento "brutas", somente com os atributos e metodos definidos.

---
+++ portalAluno/src/main/java/controller/FormaPagamento.java
@@ -0,0 +1,21 @@
+package controller;
+public class FormaPagamento {
+    private int numeroPagamento;
+    public int pix;
+    public int boleto;
+    public int cartao;
+    public double dinheiro;
+    public double cheque;
+    public double deposito;
+    public double transferencia;
+    
+    public void gerarPix(){
+        
+    }
+    public void gerarBoleto(){
+        
+    }
+    public void gerarComprovante(){
+        
+    }
+}
