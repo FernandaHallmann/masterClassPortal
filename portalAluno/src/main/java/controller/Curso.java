d9ebc8a738af762ad91cc43ac90f38d530fa8d1e
Renan Augusto Mendes Carlos <114965499+RenanAUG@users.noreply.github.com>
2023-03-30 22:18:34 -0300

    Finalizando o método Cadastrar
    Adicionado uma mais uma classe, um Construtor e criado um atributo ArrayList

--- portalAluno/src/main/java/controller/Curso.java
+++ portalAluno/src/main/java/controller/Curso.java
@@ -2,36 +2,60 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.curso;
+package com.mycompany.curso2;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
+ * @analista Eduardo
+ * @desenvolvedor renan
  *
- * @author eduro
  */
 public class Curso {
-    
         private int cargaHoraria;
         private String disciplina;
         private String turnoCurso;
         private String campusAcademico;
-
-
-        public ArrayList<Object> cadastrarCurso(String disci, String turno, String campus, int carga){
-            ArrayList<Object> cadastro = new ArrayList<>();
-
-            this.disciplina = disci;
-            this.turnoCurso = turno;
-            this.campusAcademico = campus;
-            this.cargaHoraria = carga;
-
-            cadastro.add(disci);
-            cadastro.add(turno);
-            cadastro.add(campus);
-            cadastro.add(disci);
-
-            return cadastro;
+        ArrayList<Cadastro> cadastro;
+        
+        public Curso(){
+            cadastro = new ArrayList<Cadastro>();
+        }
+        
+        public void cadastrarCurso(){
+            String disci;
+            String turno;
+            String campus;
+            int carga;
+            
+            try {
+            Scanner ler = new Scanner(System.in);
+            
+            System.out.println("Informe a disciplina do curso: ");
+            disci = ler.next();
+            
+            System.out.println("Informe qual o turno do curso: ");
+            turno = ler.next();
+            
+            System.out.println("Informe qual é o campus do curso: ");
+            campus = ler.next();
+            
+            System.out.println("Informe a  carga horaria do curso: ");
+            carga = ler.nextInt();
+            
+            cadastro.add(new Cadastro(disci, turno, campus, carga));
+            
+            for(Cadastro car : cadastro){
+                System.out.println("========= Curso salvo com Sucesso!===========");
+                System.out.println("Curso: " + car.getDisciplina()+ "," + car.getTurno()+ "," + car.getCampus() + ","+ car.getCargaHoraria());
+            }
+            
+            } catch (Exception exception) {
+                System.out.println("ERRO! Informe os dados corretamente");
+                System.exit(0);
+            }
         }
 
         public void removerCurso(){
@@ -87,5 +111,53 @@ public class Curso {
         public void setCampusAcademico(String campusAcademico) {
                 this.campusAcademico = campusAcademico;
         }
- }
+           
+    private class Cadastro{
+        private String campus;
+        private String disciplina;
+        private String turno;
+        private int CargaHoraria;
 
+    public Cadastro(String campus, String disciplina, String turno, int CargaHoraria) {
+        this.campus = campus;
+        this.disciplina = disciplina;
+        this.turno = turno;
+        this.CargaHoraria = CargaHoraria;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int CargaHoraria) {
+        this.CargaHoraria = CargaHoraria;
+    }
+        
+      
+    }
+    
+}
